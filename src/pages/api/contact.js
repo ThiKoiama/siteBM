e3a327e87a7fedb8db15cd7faa0a47384a5f5d1e
ThiKoiama <thiagokoiama@gmail.com>
2022-06-21 11:02:53 -0300

    commit

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -5,7 +5,7 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 async function contact(req, res) {
   try {
     await sendgrid.send({
-      to: 'suportebmbike@gmail.com',
+      to: 'atendimento@bmbike.com.br',
       from: 'sitesbmbike@gmail.com',
       subject: `Mensagem de ${req.body.email} pelo site da BM.`,
       text: "Nome: " + req.body.nome + "\nEndereço: " + req.body.endereco + "\nEmail: " + req.body.email + "\nAssunto: " + req.body.assunto + "\nTelefone: " + req.body.telefone + "\nMensagem: " + req.body.mensagem,
